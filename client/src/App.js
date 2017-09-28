d7382db050339a818d2f14b0bbcdba85a922ecaf
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2017-09-28 09:31:44 +0545

    setup bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+import NavigationBar from './components/Pages/NavigationBar'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p className="App-intro">
-          Hello World
-        </p>
+      <div className="container">
+        <p>Hello World</p>
       </div>
     );
   }
