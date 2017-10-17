12c2764364058309e19cc3893ccac4429f87dd5d
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2017-10-17 09:46:06 +0545

    Add missing migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171017040041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "plutus_accounts", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.boolean "contra"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["name", "type"], name: "index_plutus_accounts_on_name_and_type"
+  end
+
+  create_table "plutus_amounts", id: :serial, force: :cascade do |t|
+    t.string "type"
+    t.integer "account_id"
+    t.integer "entry_id"
+    t.decimal "amount", precision: 20, scale: 10
+    t.index ["account_id", "entry_id"], name: "index_plutus_amounts_on_account_id_and_entry_id"
+    t.index ["entry_id", "account_id"], name: "index_plutus_amounts_on_entry_id_and_account_id"
+    t.index ["type"], name: "index_plutus_amounts_on_type"
+  end
+
+  create_table "plutus_entries", id: :serial, force: :cascade do |t|
+    t.string "description"
+    t.date "date"
+    t.integer "commercial_document_id"
+    t.string "commercial_document_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["commercial_document_id", "commercial_document_type"], name: "index_entries_on_commercial_doc"
+    t.index ["date"], name: "index_plutus_entries_on_date"
+  end
 
 end
