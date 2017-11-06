f869147c646de51a7fb978bd7c9342d71001d254
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2017-11-06 09:44:30 +0545

    fixed filing spec, add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :entries, except: :destroy
+  resources :assets, except: [:destroy, :index]
+  resources :equities, except: [:destroy, :index]
+  resources :liabilities, except: [:destroy, :index]
+  resources :revenues, except: [:destroy, :index]
+  resources :expenses, except: [:destroy, :index]
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 require 'simplecov'
+
 ENV['RAILS_ENV'] ||= 'test'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' if ENV['COVERAGE']
+
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
@@ -12,18 +14,6 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
